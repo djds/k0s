72c75e2d8cc0835231e242455b9899ab09e3797b
Davis Schirmer <djds@bghost.xyz>
2022-03-05 13:40:14 -0500

    Shellcheck hack/get-calico.sh; use idempotent rm -f

--- hack/get-calico.sh
+++ hack/get-calico.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 
 set -e
+
 CSPLIT_BINARY="csplit"
 
-# on MacOS, we need to use the homebrew coreutils (gnu-utils) version of csplit, named gcsplit by default
-if [[ "$OSTYPE" == "darwin"* ]]; then
+# On MacOS, we need to use the homebrew coreutils (gnu-utils) version of
+# csplit, named gcsplit by default.
+if [[ "${OSTYPE}" == 'darwin'* ]]; then
     CSPLIT_BINARY="gcsplit"
 fi
 
-PATH=$PATH:$GOPATH/bin
+PATH="${PATH}:${GOPATH}/bin"
 
 DIR="static/manifests/calico"
 
-mkdir -p $DIR
+mkdir -p "${DIR}"
 
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
+curl -O 'https://docs.projectcalico.org/manifests/calico.yaml'
 
-$CSPLIT_BINARY --digits=2 --quiet --prefix=$DIR/ calico.yaml "/---/" "{*}"
+"${CSPLIT_BINARY}" --digits=2 --quiet --prefix="${DIR}/" calico.yaml "/---/" "{*}"
 
-for f in $DIR/*
-do
+for f in "${DIR}/"*; do
     # skip directories
-    if [ -d $f ]; then
+    [ -d "${f}" ] && continue
+
+    filename="$(yq eval '.metadata.name' "${f}")"
+    kind="$(yq eval '.kind' "${f}")"
+
+    if [[ "${filename}" == "null" || "${kind}" == "null" ]]; then
+        rm -f "${f}"
         continue
     fi
-
-    filename=$(yq eval '.metadata.name' $f)
-    kind=$(yq eval '.kind' $f)
-
-    if [[ $filename == "null" || $kind == "null" ]]; then
-        rm $f
-        continue
-    fi
-    echo "Processing $kind $filename $f"
-    mkdir -p $DIR/$kind
-    mv $f $DIR/$kind/$filename.yaml
+    echo "Processing ${kind} ${filename} ${f}"
+    mkdir -p "${DIR}/${kind}"
+    mv "${f}" "${DIR}/${kind}/${filename}.yaml"
 done
 
 
 # cleanup
-rm calico.yaml
+rm -f calico.yaml
